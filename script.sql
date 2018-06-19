9a1f6f07198d523ec3077c046ca1f21d9d366c86
STISR <catastrofee.13@gmail.com>
2018-06-19 17:14:29 +0800

    third commit

---
+++ script.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    fName VARCHAR(50),
+    lName VARCHAR(50),
+    add VARCHAR(200),
+    phoneNumber int(50),
+    email VARCHAR(50),
+    birthdate VARCHAR(10),
+    sex VARCHAR,
+    experience VARCHAR(50),
+    conPerson VARCHAR(50),
+    conNum int(50),
+    pw1 VARCHAR,
+    pw2 VARCHAR
+    );
+INSERT INTO users (    
+);
